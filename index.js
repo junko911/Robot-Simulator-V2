7337bafac905dfb1f671961de40cde6c894f2e5c
junko911 <junkotahara911@gmail.com>
2020-08-26 22:02:31 -0400

    Finish deliverables

--- index.js
+++ index.js
@@ -1,6 +1,52 @@
 document.addEventListener("DOMContentLoaded", function(){
+  
+  const container = document.getElementById('moves-container')
+  
+  const keyHandler = () => {
+    document.addEventListener('keydown', e => {
+      if (e.key === 'ArrowLeft') {
+        renderDirection('left')
+      } else if (e.key === 'ArrowRight') {
+        renderDirection('right')
+      } else if (e.key === 'ArrowDown') {
+        renderDirection('down')
+      } else if (e.key === 'ArrowUp') {
+        renderDirection('up')
+      } else if (e.key === 'Backspace') {
+        if (container.lastChild) {
+          container.lastChild.remove()
+        }
+      }
+    })
+  }
+  
+  const renderDirection = direction => {
+    const li = document.createElement('li')
+    li.textContent = direction
+    container.append(li)
+  }
+  
+  const clickHandler = () => {
+    document.getElementById('move-button').addEventListener('click', e => {
+      const direction = document.getElementById('moves-container')
+      if (direction.childElementCount > 0) {
+        const moveBySecond = setInterval(() => {
+          if (direction.childElementCount > 0) {
+            move(direction.firstChild.textContent)
+            direction.firstChild.remove()
+          } else {
+            alert('Out of moves')
+            clearInterval(moveBySecond)
+          }
+        }, 1000)
+      } else {
+        alert('Out of moves')
+      }
+    })
+  }
+
   createGrid()
   renderBot(currentPosition)
-
-  // ADD CODE HERE!
+  keyHandler()
+  clickHandler()
 })
